70f088a1f8943c4fbc71703363bd4654ae4c0e1b
Creak5 <creak55@gmail.com>
2015-10-13 14:55:44 +0300

    Update iframe-whitelist.js

--- assets/javascripts/iframe-whitelist.js
+++ assets/javascripts/iframe-whitelist.js
@@ -1,7 +1,4 @@
 (function() {
-  /*Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/www\.freesound\.org\/embed\/sound\/iframe\/.+/i);
-  Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/w\.soundcloud\.com\/player\/.+/i);
-  Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/graphics8\.nytimes\.com\/.+/i);*/
   Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/vk\.com\/video_ext\.php\/.+/i);
 })();
 
